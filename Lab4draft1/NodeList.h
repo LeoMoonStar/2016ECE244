23d44cef5d4351d68566e086a9b21a5ec6e29e76
LeoMoonStar <lostmoonstar@gmail.com>
2016-11-08 12:27:44 -0500

    Update NodeList.h

--- Lab4draft1/NodeList.h
+++ Lab4draft1/NodeList.h
@@ -1,8 +1,17 @@
-#pragma once
+#ifndef NODE_LIST.H
+#defin NODE_LIST.H
+#include"Node.h"
+#include"ResistorList"
+
 class NodeList
 {
+private:
+	Node node;
+	int id;
+	ResistorList Rlist;
+	Node* next;
 public:
 	NodeList();
 	~NodeList();
 };
-
+#endif
