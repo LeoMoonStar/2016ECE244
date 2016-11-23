d9e0e06b925daad330003cd0e2e44e0478b6b4a2
LeoMoonStar <lostmoonstar@gmail.com>
2016-11-22 22:02:18 -0500

    Add files via upload

--- Lab5/TreeDB.cpp
+++ Lab5/TreeDB.cpp
@@ -1,32 +1,112 @@
 #include "TreeDB.h"
 
-
-
-TreeDB::TreeDB()
-{
-	root = NULL;
-	probesCount = 0;
+TreeDB::TreeDB() {
+    root = NULL;
+    probesCount = 0;
 }
 
 bool TreeDB::insert(DBentry *newEntry) {
-	if (root == NULL)
-		root->getEntry()->
-
+    if (root == NULL) {
+        TreeNode newNode(newEntry);
+        root = &newNode;
+    } else {
+        TreeNode * temp(newEntry);
+        TreeNode* current = root;
+        while (current->getEntry()->getName() != NULL) {
+            if (nameSame(temp->getEntry()->getName(), current->getEntry()->getName()))
+                return false;
+            else {
+                if (frontOneBigger(temp->getEntry()->getName(), current->getEntry()->getName()))
+                    current = current->getRight();
+                else 
+                    current =current->getLeft();
+        }
+    }
+        current=temp;
+        return true;
+}
 }
 
 DBentry TreeDB::find(string name) {
+    DBentry *currentEntry;
+    TreeNode* current=root;
+    while(current->getEntry()->getName() != NULL){
+        if (nameSame(name, current->getEntry()->getName()))
+                currentEntry=current->getEntry();
+            else {
+                if (frontOneBigger(name, current->getEntry()->getName()))
+                    current = current->getRight();
+                else 
+                    current=current->getLeft();
+            }
+    }
+    return currentEntry;
+}
+void TreeDB::clear(){
+    
+}
+void TreeDB::printProbes(){
+    
+}
+void TreeDB::countActive() const{
+    
+}
+ostream&operator<<(ostream& out, const TreeDB& rhs){
+    
+}
 
+TreeNode *TreeDB::returnRoot() {
+    return root;
+}
+
+int TreeDB::getprobes() {
+    return probesCount;
 }
 
 
 
+TreeDB::~TreeDB() {
+    TreeNode *current = root;
+    while (current->getLeft() != NULL) {
 
-TreeDB::~TreeDB()
-{  
-	TreeNode *current = root;
-	while(current->getLeft() != NULL)
-	{
-
-	}
+    }
 }
 
+bool frontOneBigger(string a, string b) {
+    int size_a = a.size();
+    int size_b = b.size();
+    int count = 0;
+    bool frontBigger = true;
+    while ((count <= size_a) && (count <= size_b) && frontBigger) {
+        if (a[count] < b[count])
+            frontBigger = false;
+        else if (a[count] == b[count])
+            count++;
+        else
+            return frontBigger;
+    }
+    return frontBigger;
+}
+
+bool nameSame(string a, string b) {
+    if (a == b)
+        return true;
+    else
+        return false;
+}
+
+bool TreeDB::findit(string name){
+    DBentry *currentEntry;
+    TreeNode* current=root;
+    while(current->getEntry()->getName() != NULL){
+        if (nameSame(name, current->getEntry()->getName()))
+                return true;
+            else {
+                if (frontOneBigger(name, current->getEntry()->getName()))
+                    current = current->getRight();
+                else 
+                    current=current->getLeft();
+            }
+    }
+    return false;
+}
--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -5,110 +5,127 @@
 #include<sstream>
 using namespace std;
 
-void functionSelect(string line,TreeDB *a);
+void functionSelect(string line, TreeDB *a);
 bool frontOneBigger(string a, string b);
-bool insert(string line, TreeDB *a);
-bool find(string line, TreeDB *a);
-bool remove(string line, TreeDB *a);
-bool printall(TreeDB *a);
-bool printprobes(string line, TreeDB *a);
-bool removeall(TreeDB *a);
-bool countactive(TreeDB *a);
-bool updatestatus(string line, TreeDB *a);
-
-
+void insert(string line, TreeDB *a);
+void find(string line, TreeDB *a);
+void remove(string line, TreeDB *a);
+void printall(TreeDB *a);
+void printprobes(string line, TreeDB *a);
+void removeall(TreeDB *a);
+void countactive(TreeDB *a);
+void updatestatus(string line, TreeDB *a);
 
 int main() {
-	TreeDB tdb;
-	TreeDB* a = &tdb;
-	string line;
-	cout << "> ";
-	getline(cin, line);
-	while (!cin.eof) {
-		functionSelect(line);
-		cout << "> ";
-		getline(cin, line);
-	}
-	
+    TreeDB tdb;
+    TreeDB* a = &tdb;
+    string line;
+    cout << "> ";
+    getline(cin, line);
+    while (!cin.eof) {
+        functionSelect(line);
+        cout << "> ";
+        getline(cin, line);
+    }
 
-	return 0;
+
+    return 0;
 }
 
-
-
-void functionSelect(string line,TreeDB *a) {
-	string command;
-	stringstream ss(line);
-	ss >> command;
-	if (command == "insert")
-		insert(line,a);
-	else if (command == "find")
-		find(line,a);
-	else if (command == "remove")
-		remove(line,a);
-	else if (command == "printall")
-		printall(a);
-	else if (command == "printprobes")
-		printprobes(line,a);
-	else if (command == "removeall")
-		removeall(a);
-	else if (command == "countactive")
-		countactive(a);
-	else if (command == "updatestatus")
-		updatestatus(line,a);
-	else
-		cout << "Invalid command" << endl;
+void functionSelect(string line, TreeDB *a) {
+    string command;
+    stringstream ss(line);
+    ss >> command;
+    if (command == "insert")
+        insert(line, a);
+    else if (command == "find")
+        find(line, a);
+    else if (command == "remove")
+        remove(line, a);
+    else if (command == "printall")
+        printall(a);
+    else if (command == "printprobes")
+        printprobes(line, a);
+    else if (command == "removeall")
+        removeall(a);
+    else if (command == "countactive")
+        countactive(a);
+    else if (command == "updatestatus")
+        updatestatus(line, a);
+    else
+        cout << "Invalid command" << endl;
 }
 
-bool insert(string line, TreeDB *a) {
-	string command, name;
-	int ipAddress;
-	bool status;
-	stringstream ss(line);
-	ss >> command >> name >> ipAddress >> status;
+void insert(string line, TreeDB *a) {
+    string command, name;
+    unsigned int ipAddress;
+    bool status;
+    stringstream ss(line);
+    ss >> command >> name >> ipAddress >> status;
+    DBentry newEntry(name, ipAddress, status);
+    DBentry *b = &newEntry;
+    if (a->insert(b))
+        cout << "Success" << endl;
 
-}
-bool find(string line, TreeDB *a) {
-	string command, name;
-	stringstream ss(line);
-	ss >> command >> name;
-}
-bool remove(string line, TreeDB *a) {
-	string command, name;
-	stringstream ss(line);
-	ss >> command >> name;
-}
-bool printall(TreeDB *a) {
-
-}
-bool printprobes(string line, TreeDB *a) {
-
-}
-bool removeall(TreeDB *a) {
-
-}
-bool countactive(TreeDB *a) {
-
-}
-bool updatestatus(string line, TreeDB *a) {
-
+    else
+        cout << "Error: entry already exists" << endl;
 }
 
-
-
-
-bool frontOneBigger(string a, string b) {
-	int size_a = a.size();
-	int size_b = b.size();
-	int count = 0;
-	bool frontBigger = true;
-	while ((count <= size_a) && (count <= size_b) && frontBigger) {
-		if (a[count] < b[count])
-			frontBigger = false;
-		else if (a[count] == b[count])
-			count++;
-		else
-			return frontBigger;
-	}
-	return frontBigger;
+void find(string line, TreeDB *a) {
+    string command, name;
+    stringstream ss(line);
+    ss >> command >> name;
+    if (a->findit(name)) {
+        DBentry findEntry(a->find(name));
+        cout << findEntry << endl;
+    } else
+        cout << "Error: entry does not exist" << endl;
 }
+
+void remove(string line, TreeDB *a) {
+    string command, name;
+    stringstream ss(line);
+    ss >> command >> name;
+    if (a->remove(name))
+        cout << "Success" << endl;
+    else
+        cout << "Error: entry does not exist" << endl;
+}
+
+void printall(TreeDB *a) {
+    cout << a;
+}
+
+void printprobes(string line, TreeDB *a) {
+    string command, name;
+    stringstream ss(line);
+    ss >> command>>name;
+    if (a->findit(name)) {
+        DBentry findEntry(a->find(name));
+        a->printProbes();
+    } else
+        cout << "Error: entry does not exist" << endl;
+}
+
+void removeall(TreeDB *a) {
+    a->clear();
+    cout << "Success" << endl;
+
+}
+
+void countactive(TreeDB *a) {
+    a->countActive();
+}
+
+void updatestatus(string line, TreeDB *a) {
+    string command, name;
+    bool status;
+    stringstream ss(line);
+    ss >> command >> name>>status;
+    if (a->findit(name)) {
+        DBentry *findOne = a->find(name);
+        findOne->setActive(status);
+    } else
+        cout << "Error: entry does not exist" << endl;
+}
+
